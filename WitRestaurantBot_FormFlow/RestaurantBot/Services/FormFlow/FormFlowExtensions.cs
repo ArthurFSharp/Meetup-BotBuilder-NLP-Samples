dce4b1570e692e91e10204b6772273a1db2142d0
Timothé Larivière <timothe.lariviere@outlook.com>
2018-04-25 16:03:44 +0200

    FormFlow

--- WitRestaurantBot_FormFlow/RestaurantBot/Services/FormFlow/FormFlowExtensions.cs
+++ WitRestaurantBot_FormFlow/RestaurantBot/Services/FormFlow/FormFlowExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Bot.Builder.FormFlow;
 using Microsoft.Bot.Builder.FormFlow.Advanced;
+using System.Linq;
 
 namespace RestaurantBot.Services.FormFlow
 {
@@ -8,8 +9,11 @@ namespace RestaurantBot.Services.FormFlow
         public static IFormBuilder<T> DateTimeRangeField<T>(this IFormBuilder<T> formBuilder, string name, ActiveDelegate<T> active = null, ValidateAsyncDelegate<T> validate = null)
             where T : class
         {
+            var prompt = typeof(T).GetProperty(name).GetCustomAttributes(typeof(PromptAttribute), false).Cast<PromptAttribute>().First();
+
             return formBuilder.Field(new FieldReflector<T>(name)
                                             .SetType(typeof(string))
+                                            .SetPrompt(prompt)
                                             .SetFieldDescription(name)
                                             .SetActive(active)
                                             .SetValidate(validate));
